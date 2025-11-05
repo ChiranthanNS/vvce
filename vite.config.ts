65ab58214bcff33f7788664160d699146b336b5f
unknown <hariprasadhp637@gmail.com>
2025-11-05 23:31:39 +0530

    git pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/vvce/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
